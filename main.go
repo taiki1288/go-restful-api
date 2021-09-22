8330962a16895752a1ccb4ece9b63a2cd51dd92e
taiki yamashita <taikity0808@gmail.com>
2021-09-22 18:13:03 +0900

    album構造体のスライスを作成

--- main.go
+++ main.go
@@ -4,5 +4,11 @@ type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
-	Prince float64 `json:"price"`
+	Price float64  `json:"price"`
+}
+
+var albums = []album {
+	{ID: "1", Title: "ChuchuTrain", Artist: "EXILE", Price: 5000},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 4000},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 3000},
 }
