dd3a2747d75f6f2be00217c60b4d693aa0287c14
taiki yamashita <taikity0808@gmail.com>
2021-09-22 21:29:31 +0900

    feature/main関数を作成

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
-package main 
+package main
+
+import "github.com/gin-gonic/gin"
 
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
-	Price float64  `json:"price"`
+	Price  float64 `json:"price"`
 }
 
 func getAlbums(c *gin.Context) {
@@ -13,8 +15,17 @@ func getAlbums(c *gin.Context) {
 	// c.IndentedJSONで構造体をJSONにシリアライズし、レスポンスに追加する。
 }
 
-var albums = []album {
+var albums = []album{
 	{ID: "1", Title: "ChuchuTrain", Artist: "EXILE", Price: 5000},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 4000},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 3000},
+}
+
+func main() {
+	router := gin.Default()
+	// Defaultを使用してGinルーターを初期化
+	router.GET("/albums", getAlbums)
+	// GET関数を使用してGET HTTPメソッドと/albumsパスをハンドラ関数に関連付ける。getAlbums関数を渡す。
+	router.Run("localhost:8080")
+	// Run関数を使って、ルータをhttp.Serverに接続し、サーバを起動する。
 }
